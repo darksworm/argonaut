ac3370e8303570f5ea809e1e2fedb2ee7e4a2450
ilmars <9987548+darksworm@users.noreply.github.com>
2025-09-27 21:42:23 +0200

    chore: added npm build

--- npm/runner/scripts/install.js
+++
@@ -1,5 +0,0 @@
-/* eslint-disable no-console */
-console.error('[argonaut] Using npm wrapper that launches the native binary.');
-console.error('If the binary fails to run, ensure the correct platform package is present');
-console.error('or install via brew/scoop/winget/curl: https://github.com/darksworm/argonaut#install');
-
